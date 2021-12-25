2a0dab9ca8dcdc58e9a600213e59e3dbb7287c6e
Sena Nur Erdem <snnrrdm@gmail.com>
2021-12-26 00:18:48 +0300

    Initial Commit

---
+++ 2021_guz_proje/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2021_guz_proje
+//
+//  Created by Sena Nur Erdem on 26.12.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
